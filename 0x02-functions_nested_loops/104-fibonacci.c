8e165d244ed4e324b1b8e0723e7cc211142acfde
hileamlak <hilea.buisness@gmail.com>
2020-09-21 20:19:09 +0000

    Fix 104-fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,59 +1,76 @@
 #include <stdio.h>
 
-int powB(int base, int power)
-{
-        int i, prod = 1;
-        for (i = 0; i < power; i++)
-        {
-                prod = prod * base;
-        }
-        return prod;
-}
-int numLength(int num)
-{
-        int length = 0;
-
-        while (num)
-        {
-                num = num / 10;
-                length += 1;
-        }
-
-
-        return length;
-}
-
 
 /**
- * main - print first 98 sequences of fibo
- * Return: 0;
+ *numLength - returns the lenth of string
+ *@num : operand number
+ *Return: number of digits
  */
+int numLength(int num)
+{
+	int length = 0;
 
+	if (!num)
+	{
+		return (1);
+	}
+
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+
+
+	return (length);
+}
+
+/**
+ *main - prints the first 98 fibonaci sequences
+ *Return: 0
+ */
 int main(void)
 {
-	long unsigned int f1 = 1, f2 = 2, i = 1, tmp = 0, limit = 100000000, h;
 
-	printf("%lu, %lu, ", f1, f2);
+	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f1o = 0, f2o = 0, tmpo = 0;
+	short int i = 1, initial0s;
+
 	while (i <= 98)
 	{
-		tmp = f1 + f2;
-		f1 = f2;
-		f2 = tmp;
 
-		if (f2 > limit)
+		if (f1o > 0)
 		{
-			tmp = f2 % limit;
-			h = (f2 - tmp) / powB(10, numLength(f2) - 1);
+			printf("%lu", f1o);
 		}
 
-		(f2 > )printf("%lu", f2);
-		if (i != 98){
-			printf(", ");
+		initial0s = numLength(mx) - 1 - numLength(f1);
+
+		while (f1o > 0 && initial0s > 0)
+		{
+			printf("%i", 0);
+			initial0s--;
 		}
+		printf("%lu", f1);
+
+		tmp = (f1 + f2) % mx;
+		tmpo = f1o + f2o + (f1 + f2) / mx;
+
+		f1 = f2;
+		f1o = f2o;
+
+		f2 = tmp;
+		f2o = tmpo;
+
+		if (i != 98)
+			printf(", ");
 		else
 			printf("\n");
 		i++;
 
 	}
+
+
 	return (0);
+
+
 }
