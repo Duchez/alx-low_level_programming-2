3853c84751e2389e7937a480fc14495aaa630211
Hileamlak Mulugeta Yitayew <68893442+hileamlakB@users.noreply.github.com>
2020-09-29 21:57:39 +0300

    Fix overflow problem

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  *mapint - maps a character to its integer form
  *@a: character to be mapped
@@ -29,7 +28,7 @@ int isDigit(char i)
  */
 int _atoi(char *s)
 {
-	int num = 0, sign = 1, started = 0;
+	signed long num = 0, sign = 1, started = 0;
 
 	while (*s)
 	{
