538a398f97e4ce819a47f1e23b18f40afa0d75ba
hileamlak <hilea.buisness@gmail.com>
2020-10-13 16:22:09 +0000

    Push malloc free exercise

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: width of array
+ * @height: height of array
+ * Return: double pointer to multi-dimen array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid, r, c, **_grid;
+
+	if (width < 0 || height < 0)
+	{
+		return (NULL);
+	}
+
+	grid = malloc(sizeof(int *) * height);
+	/*if memory is insifficuent*/
+	if (!grid)
+		return (NULL);
+	_grid = grid;
+
+	c = 0;
+	while (c < height)
+	{
+		*(_grid + c) = malloc(width * sizeof(int));
+
+		if (!(*(_grid + c)))
+			return (NULL);
+
+		r = 0;
+		while (r < width)
+		{
+			*(*(_grid + c) + r) = 0;
+			r++;
+
+		}
+		c++;
+	}
+
+	return (grid);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+# 0x0B. C - malloc, free
+Practicing malloc with c
