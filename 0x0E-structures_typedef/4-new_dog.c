76cfd9f2d346138da3161711733dce9266442472
hileamlak <hilea.buisness@gmail.com>
2020-10-19 12:03:27 +0000

    Try a fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <string.h>
 /**
  * new_dog - creates a new dog
  * @name: name of dog
@@ -14,53 +14,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (!new_dog || !name || !owner)
 		return (NULL);
-	n = malloc(_strlen(name) + 1);
+	n = malloc(strlen(name) + 1);
 	if (!n)
 		return (free(new_dog), NULL);
-	n = _strdup(name);
+	n = strdup(name);
 	new_dog->name = n;
-	o = malloc(_strlen(owner) + 1);
+	o = malloc(strlen(owner) + 1);
 	if (!o)
 		return (free(new_dog->name), free(new_dog), NULL);
-	o = _strdup(owner);
+	o = strdup(owner);
 	new_dog->owner = o;
 	new_dog->age = age;
 	return (new_dog);
 }
 
-/**
- * _strlen - returns the length of a string
- * @s: string s
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	char *p = s;
-
-	while (*s)
-		s++;
-	return (s - p);
-}
-
-/**
- * _strdup - returns a pointer to a newly allocated space in memory,
- * which contains a copy of the string given as a parameter.
- * @str: string to be copied
- * Return: copied string
- */
-char *_strdup(char *str)
-{
-	int i, len;
-	char *copy;
-
-	if (!str)
-		return (NULL);
-	len = _strlen(str);
-	copy = malloc(sizeof(char) * len + 1);
-	if (!copy)
-		return (NULL);
-	for (i = 0; i < len; i++)
-		copy[i] = str[i];
-	copy[i] = 0;
-	return (copy);
-}
