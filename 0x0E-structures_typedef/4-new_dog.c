572e84313a8bcceece41e4dfdd663ab17b6febe4
hileamlak <hilea.buisness@gmail.com>
2020-10-20 05:38:59 +0000

    Fix 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,37 +1,42 @@
 #include "dog.h"
+#include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
 /**
- * new_dog - creates a new dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- * Return: malloced dog_t
+ * *new_dog - create a new dog
+ * @name: string for name
+ * @age: integer for age
+ * @owner: string for owners
+ * Return: a dog or NULL
  */
+
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *n, *o;
-	dog_t *new_dog = malloc(sizeof(dog_t));
+struct dog *mydog;
+mydog = malloc(sizeof(struct dog));
 
-	if (!new_dog || !name || !owner)
-		return (NULL);
-	n = malloc(strlen(name) + 1);
-	if (!n)
-	{
-		free(new_dog);
-		return (NULL);
-	}
-	n = strdup(name);
-	new_dog->name = n;
-	o = malloc(strlen(owner) + 1);
-	if (!o)
-	{
-		free(new_dog->name);
-		free(new_dog);
-		return (NULL);
-	}
-	o = strdup(owner);
-	new_dog->owner = o;
-	new_dog->age = age;
-	return (new_dog);
+if (mydog == NULL)
+	return (NULL);
+
+mydog->name = strdup(name);
+
+if (mydog->name == NULL)
+{
+	free(mydog);
+	return (NULL);
 }
 
+mydog->owner = strdup(owner);
+
+if (mydog->owner == NULL){
+	free(mydog->name);
+	free(mydog);
+	return (NULL);
+}
+
+mydog->age = age;
+
+return (mydog);
+}
