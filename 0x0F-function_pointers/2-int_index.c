4ffbe920ae7ca9594a83be9e2632c6e77f30e481
hileamlak <hilea.buisness@gmail.com>
2020-10-21 16:58:11 +0000

    Apply fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,10 +9,10 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	register int i;
+	int i;
 
 	if (size > 0 && cmp && size)
-		for (i = 0; i < size; i++)
+		for (i = 0; i <= size; i++)
 			if (cmp(array[i]))
 				return (i);
 	return (-1);
