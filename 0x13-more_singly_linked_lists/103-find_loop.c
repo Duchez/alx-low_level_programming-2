1aadb90a5c496e675d4009f04e3346e0b3189482
hileamlak <hilea.buisness@gmail.com>
2020-11-03 14:40:42 +0000

    Puth more advanced tasks exercies

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop in a linked list
+ * @head: pointer to head of list
+ * Return: address of node where loop starts
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	return (NULL);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+# 0x13. C - More singly linked lists
+More linked list exercise with c
